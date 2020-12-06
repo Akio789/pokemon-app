bc955bd5ad9f994ea5a1eb25367ee4c275688d24
Akio Olvera <akiooarao@gmail.com>
2020-12-06 11:59:30 -0700

    Fixing SearchBar bug

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -6,18 +6,20 @@ const SearchBar: React.FC = () => {
   const [query, setQuery] = useState('');
   const handleClick = (
     allPokemon: PokemonData[],
-    updateCurrentPokemon: (pokemon: PokemonData[]) => void
+    updateCurrentPokemon: (pokemon: PokemonData[]) => void,
+    updateCurrentPage: (page: number) => void
   ) => {
     const searchResults = allPokemon.filter(({ name }) => {
       return name.includes(query.toLowerCase());
     });
+    updateCurrentPage(1);
     updateCurrentPokemon(searchResults);
   }
 
   return (
     <PokemonContext.Consumer>
       {
-        ({ allPokemon, updateCurrentPokemon }) => (
+        ({ allPokemon, updateCurrentPokemon, updateCurrentPage }) => (
           < div className="text-center">
             <input
               className="border-2 border-gray-400 rounded p-2 m-2 text-lg font-semibold"
@@ -28,7 +30,7 @@ const SearchBar: React.FC = () => {
               id="search"
               placeholder="Pokemon name" />
             <button
-              onClick={() => handleClick(allPokemon, updateCurrentPokemon!)}
+              onClick={() => handleClick(allPokemon, updateCurrentPokemon!, updateCurrentPage!)}
               className="border-2 bg-blue-500 hover:bg-blue-700 rounded-md px-4 py-1 text-lg font-semibold text-white"
             >Search</button>
           </div>
