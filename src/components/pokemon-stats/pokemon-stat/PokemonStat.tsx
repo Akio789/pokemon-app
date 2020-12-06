39ed454a15097cde4d4d41d11692224d019d3852
Akio Olvera <akiooarao@gmail.com>
2020-12-06 11:50:12 -0700

    Improving PokemonStat

--- src/components/pokemon-stats/pokemon-stat/PokemonStat.tsx
+++ src/components/pokemon-stats/pokemon-stat/PokemonStat.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Props } from './types';
 import { capitalize } from '../../../util/index';
+import { LinearProgress } from '@material-ui/core';
 
 const PokemonStat: React.FC<Props> = ({ stat }) => {
   return (
-    <div>
-      <p className="text-lg underline">{capitalize(stat.stat.name)}</p>
-      <p>{stat.base_stat}</p>
+    <div className="my-2 mx-4">
+      <p className="text-lg font-semibold">{capitalize(stat.stat.name)}</p>
+      <LinearProgress variant="determinate" value={Math.floor(stat.base_stat / 2)} />
     </div>
   )
 }
